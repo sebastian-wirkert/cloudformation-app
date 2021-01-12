8d9b9767f80eb03df5026bde052eb9bf300ad4d6
Sebastian Wirkert <sebastian.wirkert@posteo.de>
2021-01-12 21:17:11 +0100

    add appsync resolver

--- functions/dummyfunction_src/app.py
+++ functions/dummyfunction_src/app.py
@@ -1,5 +1,7 @@
-
+import json
 
 def handler(event, context):
     print(f"function with executed: {event}")
+    return json.dump(['sID': 100, 'sName': 'dummyshow', 'description': 'this is fake'])
+
 
