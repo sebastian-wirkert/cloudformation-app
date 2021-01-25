69678eee244b2ec51fe512ab7417b693a866e6a4
Sebastian Wirkert <sebastian.wirkert@posteo.de>
2021-01-25 10:19:04 +0100

    fix error in lambda return

--- functions/dummyfunction_src/app.py
+++ functions/dummyfunction_src/app.py
@@ -2,6 +2,6 @@ import json
 
 def handler(event, context):
     print(f"function with executed: {event}")
-    return json.dumps({'sID': "100", 'sName': 'dummyshow', 'description': 'this is fake'})
+    return {'sID': "100", 'sName': 'dummyshow', 'description': 'this is fake'}
 
 
