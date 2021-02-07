3bc75a4f0cee9a1b5ffe0656b00cb350ef9e560a
Sebastian Wirkert <sebastian.wirkert@posteo.de>
2021-02-07 21:09:17 +0100

    update function to interplay with graphql

--- src/functions/dummyfunction_src/app.py
+++ src/functions/dummyfunction_src/app.py
@@ -2,11 +2,11 @@ import json
 
 def handler(event, context):
     print(f"function executed with context: {context}")
-    print(f"function executed with event: {event}")
+    #print(f"function executed with event: {event}")
     dummyshow = "no show"
-    if event.field == "getShow":
+    if event["resolve"]== "query.getShow":
         dummyshow = "got Topmodel 2020"
-    elif event.field == "deleteShow":
+    elif event["resolve"] == "mutation.deleteShow":
         dummyshow = "deleted Dschungecamp"
 
     return {'sID': "100", 'sName': dummyshow, 'description': 'this is fake to test functionality'}
