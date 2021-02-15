51a17e40c1684c95343f9b99db6f3bc329607d0e
Sebastian Wirkert <sebastian.wirkert@posteo.de>
2021-02-15 22:30:28 +0100

    fix missing auth type

--- src-frontend-example/aws_config.js
+++ src-frontend-example/aws_config.js
@@ -1,15 +1,16 @@
 const aws_config={
-  "appSyncApiId": "ll4clvbt4bci7ia6ishff4jvge",
-  "appSyncApi": "arn:aws:appsync:eu-central-1:229294447586:apis/ll4clvbt4bci7ia6ishff4jvge",
-  "graphQLEndpoint": "https://el3gbld4qba5nbeczvgu5kymaa.appsync-api.eu-central-1.amazonaws.com/graphql",
-  "userPoolWebClientId": "11q5gqspd8qi39s3b1bj0ksc37",
-  "cognitoUnAuthorizedRole": "main-stack-CognitoUnAuthorizedRole-9U241V26AE3",
-  "appsyncLambdaRoleArn": "arn:aws:iam::229294447586:role/betsby-dev-AppsyncLamdaRole",
-  "cognitoAuthorizedRole": "main-stack-CognitoAuthorizedRole-1PW1HB99WZ85C",
+  "userPoolWebClientId": "onlrl17jjnh10pse8ldlg3h5q",
+  "cognitoUnAuthorizedRole": "betsby-prod-main-stack-CognitoUnAuthorizedRole-YJA17SN4FKKP",
+  "appsyncLambdaRoleArn": "arn:aws:iam::229294447586:role/betsby-prod-AppsyncLamdaRole",
+  "cognitoAuthorizedRole": "betsby-prod-main-stack-CognitoAuthorizedRole-R1V95OHJMQT5",
+  "mainGraphQLAuthenticationMethod": "AWS_IAM",
   "region": "eu-central-1",
-  "userPoolId": "eu-central-1_fPWxvD1ZI",
-  "identityPoolId": "eu-central-1:16b06800-be83-4bd1-a389-e468bb67d7a6",
-  "lambdaExecutionRoleArn": "arn:aws:iam::229294447586:role/betsby-dev-LambdaExecutionRole",
+  "userPoolId": "eu-central-1_pe16DGGnI",
+  "identityPoolId": "eu-central-1:ad138df3-3865-43a6-8e2c-3f63f1f5ee75",
+  "lambdaExecutionRoleArn": "arn:aws:iam::229294447586:role/betsby-prod-LambdaExecutionRole",
+  "appSyncApiId": "npbx36lgtjc6xfngcaq3f54xa4",
+  "appSyncApi": "arn:aws:appsync:eu-central-1:229294447586:apis/npbx36lgtjc6xfngcaq3f54xa4",
+  "graphQLEndpoint": "https://aqjieurln5e67hzovqriulekxi.appsync-api.eu-central-1.amazonaws.com/graphql",
   "identityPoolRegion": "eu-central-1"
 }
 export default aws_config
